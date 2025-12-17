5b59c5056538ff08f33829b70bb7a17dca5a224c
R YAMINI DEVI <131525177+YaminiReddipilli@users.noreply.github.com>
2025-12-17 19:37:09 +0530

    Update API base URL to production environment

--- client/src/utils/apiPaths.js
+++ client/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:5000/api/v1";
+export const API_BASE_URL = "https://personalfinancetracker-3eqq.onrender.com";
 
 export const API_ENDPOINTS = {
     AUTH: {
